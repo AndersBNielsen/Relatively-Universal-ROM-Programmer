c51d940210bfcddc0c6133f753c57b81e7488455
AndersBNielsen <forstuvning@gmail.com>
2024-06-26 23:03:10 +0200

    ENV fix

--- burnromwithReUnROMP.sh
+++ burnromwithReUnROMP.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-python3 send_command.py /dev/cu.usbserial-* 9600 03 DA 08  # 03 = erase, DA = Winbond, 08 = W27C512
+## Assuming this is run from the root of the 65uino repo with Programmer as a submodule
+python Programmer/send_command.py /dev/cu.usbserial-* 19200 03 DA 08  # 03 = erase, DA = Winbond, 08 = W27C512
 # After sending data there's a timeout before loading happens, so we need a delay
 sleep 2 
-python3 send_binary.py /dev/cu.usbserial-* 9600 build/abn6507rom.bin 32 
+python Programmer/send_binary.py /dev/cu.usbserial-* 19200 build/abn6507rom.bin 32 
 sleep 2
 #Read back the ROM we just programmed to check if it's good  
-python3 read_binary.py /dev/cu.usbserial-* 9600 verify.bin 4096 32 
+python Programmer/read_binary.py /dev/cu.usbserial-* 19200 verify.bin 4096 32
 diff -s build/abn6507rom.bin verify.bin
