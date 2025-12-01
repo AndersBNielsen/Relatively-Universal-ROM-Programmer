fcb529fcb194593af11cc150796147777175a0f5
Mike G <mkgin@users.noreply.github.com>
2025-12-01 17:31:40 +0200

    So far this works for reading my HN462532G... Did a little cleaning will test again later commit again if I broke something.
    * Arduino firmware
      * Changes necessary to read TMS2532 / Hitachi HN462532[G]
        * dumpROM()
          * receive extra data regarding exceptional pinout
          * continue without exceptional pinout if nothing recieved after 2x SERIAL_SETTLE_US
        * readAddress()
          * use ROM_CE(pin 18/24) as A11 for TMS2532 and like
        * latchAddress()
          * Set A11(pin 21/24) low on read  for TMS2532 and like
            * (TODO read only check, and hand write differently)
      * Using Flycheck (flycheck-arduino-setup) in emacs to keep consistent style
        * it seems the previous committers used "gnu style" or something close so will use gnu style
        * Removed trailing spaces before newlines here and there.
      * Might have been easier to debug with the i2c display, but timing differences in the code were sufficient up to now
    * read_binary.py
      * Optional exceptional_pinout parameter TMS2532
      * some debugging / timing

--- read_binary.py
+++ read_binary.py
@@ -2,21 +2,30 @@ import sys
 import time
 import serial
 
-def get_file(serial_port, baud_rate, file_path, total_bytes, block_size, rom_pincount):
+def get_file(serial_port, baud_rate, file_path, total_bytes, block_size, rom_pincount, nonstandard_pinout):
     try:
         # Open serial port
+        print("Open Serial port and Sleep 4s")
         ser = serial.Serial(serial_port, baud_rate)
         time.sleep(4)
-
+        print("Sleep Done")
         ser.write(b'\xAA') # Command byte
         # Send block size as a byte
         ser.write(b'\x01') # Command
         ser.write(bytes([block_size])) # Block size
         ser.write(b'\x00') #LSB
         ser.write(b'\x00') #MSB
-        stoppage = (total_bytes >> 8) & 0xFF 
+        stoppage = (total_bytes >> 8) & 0xFF
         ser.write(bytes([stoppage])) # Stoppage
         ser.write(bytes([rom_pincount])) # ROM pin count
+        # if nonstandard_pinout > 0  pincount is 24 and total_bytes 4096
+        # it might be a TMS2532 type, check options and write 1 or 0 or do nothing
+        if (nonstandard_pinout > 0) :
+            ser.write(b'\x01')
+            print("DEBUG: NON_STANDARD_SENT")
+            # It seems that the previous (not my version) of arduino code
+            # doesn't notice or care about the extra byte
+        print("Starting Read")
         start_time = time.time()
 
         # Initialize a buffer to store received data
@@ -24,20 +33,27 @@ def get_file(serial_port, baud_rate, file_path, total_bytes, block_size, rom_pin
 
         try:
             bytes_received = 0
+            print("Begin while True")
             while True:
+                
                 if (ser.inWaiting() > 0):
                     # Read one byte with timeout
                     byte = ser.read(1)
+                    #print("B")
 
                     # Check if start of frame is received
                     if byte == b'\xAA':
-                        #print(".", end="")
+                        # print(".", end="")
                         # Read specified block size bytes
                         frame = ser.read(block_size)
                         buffer.extend(frame)
                         bytes_received += len(frame)  # Update total bytes received
+                        # print(f"Received { bytes_received } of { total_bytes } bytes", flush=True)
+                        print("F",end="", flush=True)
                     
                     if bytes_received >= total_bytes:
+                        print("")
+                        print("DONE", flush=True)
                         break
 
             end_time = time.time()
@@ -60,8 +76,8 @@ def get_file(serial_port, baud_rate, file_path, total_bytes, block_size, rom_pin
 
 if __name__ == "__main__":
     # Check if correct number of command line arguments is provided
-    if len(sys.argv) != 7:
-        print("Usage: python read_binary.py <serial_port> <baud_rate> <file_path> <total_bytes> <block_size> <rom pincount>")
+    if ( len(sys.argv) < 7) or ( len(sys.argv) > 8 ) :
+        print("Usage: python read_binary.py <serial_port> <baud_rate> <file_path> <total_bytes> <block_size> <rom pincount> [nonstandard_pinout STRING]")
         sys.exit(1)
 
     # Extract command line arguments
@@ -71,6 +87,20 @@ if __name__ == "__main__":
     total_bytes = int(sys.argv[4])
     block_size = int(sys.argv[5])
     rom_pincount = int(sys.argv[6])  # Convert string to int
+    
+    # Test valid nonstandard_pinout strings here
+    # "TMS2532" if invalid, then exit
+    # FIXME
+    if len(sys.argv) != 8 :
+        nonstandard_pinout = 0;
+    if len(sys.argv) == 8 :
+        match sys.argv[7] :
+            case "TMS2532":
+               nonstandard_pinout = 1
+            # add new cases here
+            case _:
+               print("Invalid nonstandard_pinout STRING")
+               sys.exit(1)
 
     # Call the function to send the file
-    get_file(serial_port, baud_rate, file_path, total_bytes, block_size, rom_pincount)
+    get_file(serial_port, baud_rate, file_path, total_bytes, block_size, rom_pincount, nonstandard_pinout)
